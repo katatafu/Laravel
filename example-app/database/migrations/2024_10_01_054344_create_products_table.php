d7fb5e2fd0e02c9e42770fa5868a4b33492d3597
alena_liskova <liskova.aja@gmail.com>
2024-10-08 08:14:53 +0200

    fujky

--- example-app/database/migrations/2024_10_01_054344_create_products_table.php
+++ example-app/database/migrations/2024_10_01_054344_create_products_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('color');
+            $table->string('size');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
+            
         });
     }
 
